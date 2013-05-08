ab0fb90c77a8c34341a4fb571a9cabeb79c2acfe
mtx <thematriculated@gmail.com>
2013-05-07 22:30:04 -0500

    added analytics

--- index.php
+++ index.php
@@ -125,6 +125,12 @@ function getsheet($odnumber) {
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
+        <script>
+            var _gaq=[['_setAccount','UA-19653275-5'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
 
     </body>
 </html>
